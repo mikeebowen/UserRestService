8315c8c3bdf48c18905bb60da602a420c89c7eec
Michael Bowen <mikee.bowen@gmail.com>
2020-08-09 08:27:07 -0700

    add mapping functions

---
+++ UserRepository/Models/UserDTO.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDatabase.Models;
+
+namespace UserRepository.Models
+{
+    public class UserDTO
+    {
+        private static MapperConfiguration config = new MapperConfiguration(c => c.CreateMap<User, UserDTO>().ReverseMap());
+        private static IMapper mapper = config.CreateMapper();
+        public static List<UserDTO> GetAll()
+        {
+            List<User> users = DatabaseManager.Instance.User.ToListAsync().Result;
+            return users.Select(u => convertToUserDTO(u)).ToList();
+        }
+        public static UserDTO GetUserDTO(int id)
+        {
+            User user = DatabaseManager.Instance.User.FindAsync(id).Result;
+            return convertToUserDTO(user);
+        }
+        private static UserDTO convertToUserDTO(User user)
+        {
+            return mapper.Map<User, UserDTO>(user);
+        }
+    }
+}
--- UserRestService/Models/User.cs
+++ UserRestService/Models/User.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using UserRepository.Models;
 
 namespace UserRestService.Models
 {
@@ -16,5 +18,21 @@ namespace UserRestService.Models
         public string UserEmail { get; set; }
         public string UserPassWord { get; set; }
         public DateTime CreatedDAte { get; private set; }
+        private static MapperConfiguration config = new MapperConfiguration(c => c.CreateMap<User, UserDTO>().ReverseMap());
+        private static IMapper mapper = config.CreateMapper();
+        public static IEnumerable<User> GetAll()
+        {
+            List<UserDTO> userDTOs = UserDTO.GetAll();
+            return userDTOs.Select(userDTO => convertToUser(userDTO));
+        }
+        public static User GetUser(int id)
+        {
+            UserDTO userDTO = UserDTO.GetUserDTO(id);
+            return convertToUser(userDTO);
+        }
+        private static User convertToUser(UserDTO userDTO)
+        {
+            return mapper.Map<UserDTO, User>(userDTO);
+        }
     }
 }
